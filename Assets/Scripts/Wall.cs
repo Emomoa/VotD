8093b06ab4483b5c9d060a0a895765dd3417b197
Emomoa <113351340+Emomoa@users.noreply.github.com>
2024-10-21 13:03:32 +0200

    Update Wall.cs

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -11,17 +11,26 @@ public class Wall : MonoBehaviour
     private AudioSource hit;
     [SerializeField]
     private AudioSource scrape;
+    [SerializeField] private float n;
+    [SerializeField] private float e;
+    [SerializeField] private float s;
+    [SerializeField] private float w;
 
     // Start is called before the first frame update
     void Start()
     {
+        n = gameObject.transform.position.x + (gameObject.transform.lossyScale.x / 2);
+        e = gameObject.transform.position.z + (gameObject.transform.lossyScale.z / 2);
+        s = gameObject.transform.position.x - (gameObject.transform.lossyScale.x / 2);
+        w = gameObject.transform.position.z - (gameObject.transform.lossyScale.z / 2);
+
         //audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (isColliding && playerMovement.moveDirection.magnitude > 0.1f)
+        if (isColliding && playerMovement.isMoving)
         {
             if (!scrape.isPlaying)
             {
@@ -29,7 +38,7 @@ public class Wall : MonoBehaviour
                 //audioSource.pitch = playerMovement.moveDirection.magnitude/5;
                 scrape.Play();
             }
-        }*/ /*else
+        } /*else
         {
             scrape.Stop();
         }*/
@@ -40,6 +49,26 @@ public class Wall : MonoBehaviour
         Debug.Log("Collision");
         if (collision.gameObject.CompareTag("Player"))
         {
+            if (collision.gameObject.transform.position.x > n || collision.gameObject.transform.position.x < s)
+            {
+                hit.panStereo = 0;
+                scrape.panStereo = 0;
+            }
+            else
+            {
+                if (collision.gameObject.transform.position.z > e)
+                {
+                    hit.panStereo = 1;
+                    scrape.panStereo = 1;
+                }
+                else
+                {
+                    hit.panStereo = -1;
+                    scrape.panStereo = -1;
+                }
+            }
+
+
             //audioSource.Play();
             Debug.Log("Player collision");
             hit.Play();
