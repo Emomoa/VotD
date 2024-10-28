9b6a8c24bebf22f1a22086f6bc6de1ccb8624455
dhamaosta <viktor03.eriksson@gmail.com>
2024-10-28 15:16:05 +0100

    asdasd

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -9,8 +9,6 @@ public class Door : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip doorLocked;
     public AudioClip doorOpen;
-    public AudioClip doorCreak;
-
 
     public string nextSceneName; // �ndra fr�n Scene till string
 
@@ -31,6 +29,16 @@ public class Door : MonoBehaviour
         {
             PingDoor();
         }
+        if(pingAmount<maxPings)
+        {
+            timeTillNextPing += Time.deltaTime;
+        }
+        if(timeTillNextPing > pingInterval)
+        {
+            timeTillNextPing = 0;
+            pingAmount++;
+        }
+    }
 
     private void PingDoor()
     {
@@ -67,10 +75,5 @@ public class Door : MonoBehaviour
                 audioSource.PlayOneShot(doorLocked);
             }
         }
-    }
-
-    public void Open()
-    {
-        audioSource.PlayOneShot(doorCreak);
     }
 }
